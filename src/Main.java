91cab129b781c4345071265a9403f6a1ef57e8bd
Jean-Killian <killian.goulet.dubasque@gmail.com>
2024-10-07 17:01:35 +0200

    for loop and ramdom value

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,15 @@ public class Main {
         System.out.println("Hello world!");
         int score = 24;
         int [] tabScore = new int [7];
-        tabScore[0] = 1;
-        tabScore[1] = 2;
-        tabScore[2] = 3;
-        tabScore[3] = 4;
-        tabScore[4] = 5;
-        tabScore[5] = 6;
-        tabScore[6] = 7;
+
+        for (int i = 0; i < tabScore.length; i++) {
+            tabScore[i] = (int) Math.ceil(Math.random() * 100);
+        }
 
         for (int i = 0; i < tabScore.length; i++) {
             System.out.println(tabScore[i]);
         }
+
         int moyenne = 0;
         moyenne = (tabScore[0] + tabScore[1] + tabScore[2] + tabScore[3] + tabScore[4] + tabScore[5] + tabScore[6]) / tabScore.length;
         System.out.println("moyenne = " + moyenne);
