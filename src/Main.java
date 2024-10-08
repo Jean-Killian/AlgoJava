6ef8f4f44a868ce393002590843aefb1dadbe57b
Jean-Killian <killian.goulet.dubasque@gmail.com>
2024-10-08 11:45:19 +0200

    add new fonctions and add javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,78 @@
+/**
+ * @author killian
+ * @version v69.420
+ */
+
 public class Main {
+
+    /**
+     * Fonction return true si une des valeur du tableau est inférieur à 10
+     * @param tabScore Tableau de tout les score
+     * @return boolean true si une valeur inférieur à 10 dans le tableau
+     */
+    public static boolean isValueLow(int[]tabScore){
+        boolean isLow = false;
+        for (int i = 0; i < tabScore.length; i++) {
+            if (tabScore[i] < 10) {
+                isLow = true;
+                break;
+            }
+            else{
+                isLow = false;
+            }
+        }
+        return isLow;
+    }
+
+    /**
+     * Fonction return la valeur maximale du tableau
+     * @param tabScore Tableau de tout les scores
+     * @return int La valeur la plus élevé du tableau
+     */
+    public static int maxScore(int[]tabScore){
+
+        int maxScore = 0;
+        for (int i = 0; i < tabScore.length; i++){
+            if (tabScore[i] > maxScore){
+                maxScore = tabScore[i];
+            }
+        }
+        return maxScore;
+    }
+
+    /**
+     * Fonction Print tout les scores du tableau
+     * @param tabScore
+     */
+    public static void printScore(int[]tabScore) {
+        for (int i = 0; i < tabScore.length; i++) {
+            System.out.println(tabScore[i]);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int score = 24;
+        int score = 0;
         int [] tabScore = new int [7];
 
+        //Attribue une valeur random pour toutes les valeurs du tableau
         for (int i = 0; i < tabScore.length; i++) {
             tabScore[i] = (int) Math.ceil(Math.random() * 100);
         }
 
-        for (int i = 0; i < tabScore.length; i++) {
-            System.out.println(tabScore[i]);
-        }
+        printScore(tabScore);
 
+        //Calcul de la moyenne
+        int somme = 0;
+        for (int i = 0; i < tabScore.length; i++) {
+            somme += tabScore[i];
+        }
         int moyenne = 0;
-        moyenne = (tabScore[0] + tabScore[1] + tabScore[2] + tabScore[3] + tabScore[4] + tabScore[5] + tabScore[6]) / tabScore.length;
+        moyenne = somme / tabScore.length;
         System.out.println("moyenne = " + moyenne);
 
+
+        System.out.println("le plus grand score est : " + maxScore(tabScore));
+        System.out.println("Valeur en dessou de 10 : " + isValueLow(tabScore));
     }
 }
